96f0e6927c85c4fca35fe282128739aeb4a9a5f9
UtterDonkey <68461677+UtterDonkey@users.noreply.github.com>
2023-11-26 16:26:13 +0000

    Update forms.user.js

--- forms.user.js
+++ forms.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Forms Hacks
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
+// @version      0.2
+// @description  Microsoft Forms hacks script that supports Microsoft Teams.
 // @author       You
 // @match        https://forms.office.com/Pages/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=forms.microsoft.com
 // @grant        none
+// @downloadURL  https://raw.githubusercontent.com/UtterDonkey/bh-gui/main/forms.user.js
+// @updateURL    https://raw.githubusercontent.com/UtterDonkey/bh-gui/main/forms.user.js
 // ==/UserScript==
 
 (function() {
@@ -53,7 +55,8 @@
         document.querySelector('div[data-automation-id=noticeContainer]').appendChild(notice);
         const form = (await (await fetch('https://data.pixelbulb.online/forms?url=' + encodeURIComponent(location.href))).json());
         if (form.quizResult === null || form.error) {
-            const formData = (await (await fetch(`https://forms.office.com/handlers/ResponsePageStartup.ashx?id=${(new URL(location.href)).searchParams.get('id')}&mobile=false`)).json()).data.form.questions;
+            const thisForm = (await (await fetch(`https://forms.office.com/handlers/ResponsePageStartup.ashx?id=${(new URL(location.href)).searchParams.get('id')}&mobile=false`)).json()).data.form;
+            formData = thisForm.questions;
             const res = await (await fetch("https://data.pixelbulb.online/forms?questions=" + encodeURIComponent(JSON.stringify(formData.map(e => { return { id: e.id, question: e.title } }))))).json();
             Object.entries(res).forEach(result => {
                 const span = document.createElement('span');
@@ -61,7 +64,7 @@
                 document.querySelector('span#QuestionInfo_' + result[0]).insertAdjacentElement('afterend', span);
             });
 
-            notice.innerText = Object.keys(res).length > 0 ? 'This form doesn\'t fully support answers yet. Found answers are Displayed in Bold.' : 'This form doesn\'t support answers yet.';
+            notice.innerText = Object.keys(res).length > 0 ? 'This form doesn\'t fully support answers yet. Found answers are Displayed in Bold.' : (JSON.parse(thisForm.settings).IsQuizMode ? 'This form doesn\'t support answers yet.' : 'This form isn\'t a quiz.');
             hackClient();
         } else {
 
@@ -82,6 +85,7 @@
                 document.querySelector('span#QuestionInfo_' + result.id).insertAdjacentElement('afterend', span);
             });
             notice.innerText = 'Answers are Displayed in Bold.';
+            hackClient();
         }
     })();
 })();
